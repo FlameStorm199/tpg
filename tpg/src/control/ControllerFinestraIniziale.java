1a7e32d898d2acce360360c8db33d894522ad531
Emanuele <ematicon@gmail.com>
2023-11-17 08:29:46 +0100

    Label al posto del button

--- tpg/src/control/ControllerFinestraIniziale.java
+++ tpg/src/control/ControllerFinestraIniziale.java
@@ -2,12 +2,14 @@ package control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import model.Client;
 import view.FinestraIniziale;
 import view.Window;
 
-public class ControllerFinestraIniziale implements ActionListener{
+public class ControllerFinestraIniziale implements ActionListener, MouseListener{
 	private FinestraIniziale fi;
 	private Client client;
 	
@@ -17,12 +19,12 @@ public class ControllerFinestraIniziale implements ActionListener{
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) {/*
 		// TODO Auto-generated method stub
 		if(e.getSource() == fi.getButton()) {
 			System.out.println("IP: "+fi.getIP());
 			this.client = new Client(fi.getIP());
-		}
+		}*/
 	}
 	
 	public void acceptedRequest() {
@@ -40,4 +42,38 @@ public class ControllerFinestraIniziale implements ActionListener{
 		//TODO: This should be in a label
 		System.out.println("The server rejected your connection request. This usually happens because your request would exceed the maximum number of clients connected to that server.");
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == fi.getLblConnect()) {
+			System.out.println("IP: "+fi.getIP());
+			this.client = new Client(fi.getIP());
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- tpg/src/view/FinestraIniziale.java
+++ tpg/src/view/FinestraIniziale.java
@@ -24,10 +24,10 @@ public class FinestraIniziale extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JButton btnConnect;
 	private JLabel titolo;
 	private JLabel sfondoLabel;
 	private ImageIcon sfondo;
+	private JLabel lblConnect;
 	
 	/**
 	 * Create the frame.
@@ -40,22 +40,22 @@ public class FinestraIniziale extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		contentPane.setLayout(null);
-		sfondo = new ImageIcon(new ImageIcon("src/immagini/sfondoHome.jpg").getImage().getScaledInstance(1600, 800, Image.SCALE_DEFAULT));
+		sfondo = new ImageIcon(new ImageIcon("src/immagini/sfondoHome.jpg").getImage().getScaledInstance(1680, 900, Image.SCALE_DEFAULT));
+		
+		lblConnect = new JLabel("CONNECT");
+		lblConnect.setForeground(Color.WHITE);
+		lblConnect.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConnect.setFont(new Font("Arcade Normal", Font.PLAIN, 20));
+		lblConnect.setBounds(567, 369, 406, 74);
+		contentPane.add(lblConnect);
 		
 		textField = new JTextField();
+		textField.setForeground(Color.BLACK);
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
 		textField.setFont(new Font("Arcade Normal", Font.PLAIN, 15));
 		textField.setBounds(638, 301, 264, 46);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
-		btnConnect = new JButton("Connect");
-		btnConnect.setFont(new Font("Arcade Normal", Font.PLAIN, 10));
-		btnConnect.setBounds(684, 395, 172, 41);
-	    btnConnect.setContentAreaFilled(true);
-	    btnConnect.setBorderPainted(false);
-	    btnConnect.setFocusPainted(false);
-	    
-		contentPane.add(btnConnect);
 		
 		titolo = new JLabel("THE PENALTY GAME");
 		titolo.setForeground(new Color(255, 255, 255));
@@ -67,21 +67,24 @@ public class FinestraIniziale extends JFrame {
 		setContentPane(contentPane);
 		
 		sfondoLabel = new JLabel("");
-		sfondoLabel.setBounds(0, 10, 1600, 800);
+		sfondoLabel.setBounds(0, 0, 1680, 900);
 		sfondoLabel.setIcon(sfondo);
 		contentPane.add(sfondoLabel);
 	}
 	
 	public void registraEvento(ControllerFinestraIniziale fi) {
 		textField.addActionListener(fi);
-		btnConnect.addActionListener(fi);
+		lblConnect.addMouseListener(fi);
 	}
 	
 	public String getIP() {
 		return textField.getText();
 	}
-	
-	public JButton getButton() {
-		return btnConnect;
+
+	public JLabel getLblConnect() {
+		return lblConnect;
 	}
+	
+	
+	
 }
